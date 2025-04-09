41b34efe48083169d6e737157fb831f15f821523
balaram <balaramreddy1710@gmail.com>
2025-04-09 11:50:17 +0530

    [Balaram] Modified: added additional example program for rest operator

--- Day-02/rest.js
+++ Day-02/rest.js
@@ -4,3 +4,10 @@ function show(first, ...rest) {
 }
 
 show("a", "b", "c", "d");
+
+function sum(...numbers) {
+  return numbers.reduce((a, b) => a + b, 0);
+}
+
+console.log(sum(1, 2, 3)); // 6
+console.log(sum(4, 5, 6, 7)); // 22
