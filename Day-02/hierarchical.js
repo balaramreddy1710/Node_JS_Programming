8089c278c2730931e2a99a456c59e7b84d57ddff
balaram <balaramreddy1710@gmail.com>
2025-04-09 15:57:46 +0530

    [Balaram] Add: Hierarchical inheritance program

---
+++ Day-02/hierarchical.js
@@ -0,0 +1,26 @@
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name} makes a noise.`);
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log(`${this.name} barks.`);
+  }
+}
+
+class Cat extends Animal {
+  speak() {
+    console.log(`${this.name} purrs.`);
+  }
+}
+
+const d = new Dog("Bruno");
+const c = new Cat("Stella");
+d.speak();
+c.speak();
