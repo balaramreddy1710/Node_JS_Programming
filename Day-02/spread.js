eb1f45e50782021b3da725aa60bd620daf5a4428
balaram <balaramreddy1710@gmail.com>
2025-04-09 11:54:21 +0530

    [Balaram] Modified: added additional example program for spread operator

--- Day-02/spread.js
+++ Day-02/spread.js
@@ -2,3 +2,8 @@ const arr1 = [1, 2];
 const arr2 = [...arr1, 3, 4];
 
 console.log(arr2);
+
+const user = { name: "Alice", age: 25 };
+const updatedUser = { ...user, age: 26, city: "Delhi" };
+
+console.log(updatedUser);
