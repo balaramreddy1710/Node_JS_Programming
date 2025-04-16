b26e6a408bad6ada6ced9dc6c9b5541c791a6d80
balaram <balaramreddy1710@gmail.com>
2025-04-16 14:56:49 +0530

    [Balaram] Add: Program to implement closure

---
+++ Day-06/closure.js
@@ -0,0 +1,12 @@
+function outer() {
+  let count = 0;
+  return function inner() {
+    count++;
+    return count;
+  };
+}
+
+let counter = outer();
+
+console.log(counter());
+console.log(counter());
