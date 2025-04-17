9ddf60621890e092d924599cb121c0a805f0083b
balaram <balaramreddy1710@gmail.com>
2025-04-17 12:11:11 +0530

    [Balaram] Add : Program to find the first unique character in a string

---
+++ Day-07/firstUniq.js
@@ -0,0 +1,13 @@
+function firstUniqueChar(str) {
+  const count = {};
+
+  for (let char of str) count[char] = (count[char] || 0) + 1;
+
+  for (let char of str) {
+    if (count[char] === 1) return char;
+  }
+
+  return null;
+}
+
+console.log(firstUniqueChar("ikklaan"));
